0ce7f38373085cc91ff48d61053b63639231663c
ironbooster <ironbooster014@gmail.com>
2022-03-07 18:13:27 +0200

    Initial Commit

---
+++ src/main/java/com/example/boostcom/BoostComApplication.java
@@ -0,0 +1,13 @@
+package com.example.boostcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoostComApplication.class, args);
+    }
+
+}
