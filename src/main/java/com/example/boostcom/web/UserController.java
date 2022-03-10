11fc842870c1a78dd697cd800a1bcebca0ffc44e
ironbooster <ironbooster014@gmail.com>
2022-03-10 22:37:00 +0200

    Login and Registration implementation 1/3

---
+++ src/main/java/com/example/boostcom/web/UserController.java
@@ -0,0 +1,63 @@
+package com.example.boostcom.web;
+
+import com.example.boostcom.model.dto.UserDto;
+import com.example.boostcom.model.dto.UserRegisterDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+    @ModelAttribute("userRegisterDto")
+    public UserRegisterDto createRegistrationBinding() {
+        return new UserRegisterDto();
+    }
+    @ModelAttribute("userDto")
+    public UserDto createUserBinding() {
+        return new UserDto();
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+    @PostMapping("/register")
+    public String registration(@Valid UserRegisterDto userRegisterDto,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes){
+        if(bindingResult.hasErrors()){
+            return "redirect:/user/register";
+        }
+
+        return "redirect:/user/login";
+    }
+
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @PostMapping("/login")
+    public String logging(@Valid UserDto userDto,
+                          BindingResult bindingResult,
+                          RedirectAttributes redirectAttributes){
+
+        if(bindingResult.hasErrors()){
+            return "redirect:/user/login";
+        }
+
+        return "redirect:/";
+    }
+
+
+
+}
